55d0f8a188dc46de37abd0b44b8c6a6f2f8e26ab
KIm yeong hyun <gus7518@naver.com>
2022-06-23 10:04:35 +0900

    Init Blog

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -17,14 +17,14 @@
 
 /** @type {MetaConfig} */
 const metaConfig = {
-  title: "Dev Ed",
-  description: `Ed's Blog`,
-  author: "Ed",
-  siteUrl: "https://gatsby-starter-apple.netlify.app",
+  title: "Y Developer",
+  description: `사람들은 Y, 나에겐 Why`,
+  author: "portfolioExpert",
+  siteUrl: "https://portfolioExpert.github.io/",
   lang: "en",
   utterances: "sungik-choi/gatsby-starter-apple-comment",
   links: {
-    github: "https://github.com/sungik-choi/gatsby-starter-apple",
+    github: "https://github.com/portfolioExpert",
   },
   favicon: "src/images/icon.png",
 }
